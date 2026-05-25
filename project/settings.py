dbe733d80288261aeed2bed9927f2fcb3a5a309a
diya <shresthadiya59@gmail.com>
2026-05-25 16:51:13 +0545

    function based api

--- project/settings.py
+++ project/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rms',
+    
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
--- rms/views.py
+++ rms/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Category
+from .serializer import CategorySerializer
 # Create your views here.
+
+@api_view()
+def category(request):
+   category = Category.objects.all()
+   serializer = CategorySerializer(category, many=True)    # serialize: to convert queryset to json format
+   return Response(serializer.data)
+
+# create Table api
