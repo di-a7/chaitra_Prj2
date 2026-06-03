67b766f84a4bfd0f28adaf8726992216aac65ab2
diya <shresthadiya59@gmail.com>
2026-06-03 16:47:29 +0545

    token authentication

--- project/settings.py
+++ project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rms',
     
     "rest_framework",
+    'rest_framework.authtoken',
     "debug_toolbar",
     'django_filters',
 ]
@@ -136,5 +137,6 @@ INTERNAL_IPS = [
 
 REST_FRAMEWORK = {
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5
+    'PAGE_SIZE': 5,
+    'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication']
 }
